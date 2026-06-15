2d429de48c09c95acf41716ee5d948fb7e299c53
Bernard Edet Ekoli <168956332+bernard-ekoli@users.noreply.github.com>
2026-06-15 08:04:39 +0100

    Delete .next/dev directory

--- .next/dev/static/chunks/app_page_tsx_03ay17f._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0e.zb4f._.js"],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/app_watermark_page_tsx_03ay17f._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_08d_uw_._.js"],
-    source: "dynamic"
-});
